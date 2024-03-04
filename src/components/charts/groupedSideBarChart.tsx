98f9ea1697e856c2e7f3462401e0bbdae8c09524
psysm15 <psysm15@nottingham.ac.uk>
2024-03-04 17:07:08 +0000

    Add chartjs-plugin-autocolors dependency and update styles

---
+++ src/components/charts/groupedSideBarChart.tsx
@@ -0,0 +1,112 @@
+import React, {useState, useEffect, useRef} from 'react'; 
+
+import classes from '../styles/charts/sideBarChart.module.css';
+import { Bar } from 'react-chartjs-2';
+import type { ChartData, ChartOptions } from 'chart.js';
+
+
+interface Props {
+    labels: string[];
+    title?: string;
+    dataset: {label: string, data: number[]}[];
+    options?: ChartOptions<'bar'>;
+}
+
+export default function GroupedSideBarChart({labels, title, dataset, options}:Props) {
+  // Force Graph resize because of chart js (2 year old) bug :) 
+
+
+    // Dataset
+    const chart_data: ChartData<'bar'> = {
+        labels: labels,
+        datasets: dataset
+    }
+
+    // Options
+    const default_options: ChartOptions<'bar'> = {
+        responsive: true,
+        maintainAspectRatio: false,
+        indexAxis: 'y', // Set the index axis to 'y' for horizontal bars
+        
+        layout: {
+            padding: {
+                top: 4,
+                right: 4,
+                bottom: 4,
+                left: 4,
+            },
+            
+        },
+        plugins: {
+            legend: {
+                display: false,
+            },
+            title: {
+                display: false,
+            },
+
+        },
+        scales: {
+
+            x: {
+                border: {
+                    display: false,
+                },
+                grid: {
+                    display: false,
+                },
+
+                stacked: false,
+                beginAtZero: true,
+                ticks: {
+                    font: {
+                        size: 8,
+                        weight: 600,
+                    }, 
+                },
+              
+            },
+            y: {
+                border: {
+                    display: false,
+                },
+                grid: {
+                    display: false,
+                },
+                stacked: true,
+                beginAtZero: true,
+                ticks: {
+                    autoSkip: false, // Prevents skipping of labels
+                    maxRotation: 0, // Keeps labels horizontal
+                    minRotation: 0,
+                    font: {
+                        size: 10,
+                        weight: 500,
+                    },
+                },
+            },
+        },
+        interaction: {
+            intersect: false,
+            mode: 'nearest',
+        },
+            events: []
+    };
+
+
+    const chartOptions = {...default_options, ...options};
+
+
+    return (
+        // Card
+        <div className={`${classes.graphCard}`}>
+            {/* Title */}
+            {title && <h3 className={`${classes.graphTitle}`}>{title}</h3>}
+            {/* Chart */}
+            <div className={`${classes.graphContainer}`}>
+                <Bar data={chart_data} options={chartOptions} />
+            </div>
+            
+        </div>
+    )
+}
--- src/pages/dashboard/IIA/accounts.tsx
+++ src/pages/dashboard/IIA/accounts.tsx
@@ -1,30 +1,91 @@
-import React, {useState} from 'react'; 
+import React, { useEffect, useState, useRef  } from "react";
 
-import classes from '../../../components/styles/iia/accounts.module.css'
+import classes from "../../../components/styles/iia/accounts.module.css";
+// Hooks
+import useGetAccountsChartData from "../../../hooks/useGetAccountsChartData";
 
-
+// Components
 import SingleValueCard from "../../../components/cards/singleValueCard";
+import GroupedSideBarChart from "../../../components/charts/groupedSideBarChart";
 
 export default function Accounts() {
+  // Temp Values
+  // Totals
+  const totalAccountsValue = 1000000;
+  const percentageGoalCompletion = 50;
+  const changeInTotalBalanceThisMonth = 10000;
+  // Account List (Graph)
+  const {
+    chartDataset,
+    chartLabels,
+    goalData,
+    chartLoading,
+    getAccountsChartData,
+  } = useGetAccountsChartData();
 
-    const totalAccountsValue = 1000000;
+  // Temp Get Data
+  useEffect(() => {
+    getAccountsChartData();
+  }, []);
+  // const labels = Array.from(allAccountNames);
 
-    const percentageGoalCompletion = 50;
 
-    const changeInTotalBalanceThisMonth = 10000;
 
-    return (
-        <div>
-            {/* Total Accounts Value */} {/* Percentage Goal  Completion */} {/*change in total balance this month*/}
-            <div className={`${classes.header_container}`}>
-                <SingleValueCard title="Total Balance" value={totalAccountsValue} prefix='£' />
-                <SingleValueCard title="Goal Completion" value={percentageGoalCompletion} suffix='%'/>
-                <SingleValueCard title="Change in Balance" value={changeInTotalBalanceThisMonth} prefix='£' />
-            </div>
-            {/* Accounts List, Side bar chart ranked highest to lowest with earnings*/} {/* Short Activity */}
 
-            
-            <h1>Accounts</h1>
+  return (
+    <div>
+      {/* Total Accounts Value */} {/* Percentage Goal  Completion */}{" "}
+      {/*change in total balance this month*/}
+      <div className={`${classes.header_container}`}>
+        <SingleValueCard
+          title="Total Balance"
+          value={totalAccountsValue}
+          prefix="£"
+        />
+        <SingleValueCard
+          title="Goal Completion"
+          value={percentageGoalCompletion}
+          suffix="%"
+        />
+        <SingleValueCard
+          title="Change in Balance"
+          value={changeInTotalBalanceThisMonth}
+          prefix="£"
+        />
+      </div>
+      {/* Accounts List, Side bar chart ranked highest to lowest with earnings*/}{" "}
+      {/* Goal and Balance Display */}
+      <div className={`${classes.balances_container}`}>
+        <div className={`${classes.graph_balance_container}`}>
+          <GroupedSideBarChart
+            title={"Account Balances"}
+            labels={chartLabels}
+            dataset={chartDataset}
+          />
         </div>
-    )
+        {/* Goal and Balance List */}
+        <div className={`${classes.account_goal_container}`}>
+
+          <ul className={`${classes.account_goals_list}`}>
+            <li className={`${classes.account_goals}`} key={"header"}>
+              <p className={`${classes.account_goal_header}`}></p>
+              <p className={`${classes.account_goal_header}`}>Goal</p>
+              <p className={`${classes.account_goal_header}`}>Balance</p>
+              <p className={`${classes.account_goal_header}`}>%</p>
+            </li>
+            {goalData.map((goal, index) => {
+              return (
+                <li className={`${classes.account_goals}`} key={index}>
+                  <p className={`${classes.account_goal_name}`}>{goal.name}</p>
+                  <p>£{goal.goal}</p>
+                  <p>£{goal.balance}</p>
+                  <p>{goal.completion}%</p>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+      </div>
+    </div>
+  );
 }
